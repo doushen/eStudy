8fc36683876b154f91a3ef3095a3946904065df7
liyanan <20137848@qq.com>
2017-07-06 16:40:18 +0800

    min-width

--- src/components/common/sliderBarCtrl.vue
+++ src/components/common/sliderBarCtrl.vue
@@ -98,10 +98,7 @@ export default {
   }
   .slider-bar-menu {
     max-width: 80%;
-<<<<<<< HEAD
     min-width: 30%;
-=======
->>>>>>> 5fc7fedd01ff16556ecebcdfa5cd8b64bf0b0426
     z-index: 140;
     background-color: #363c43;
     position: fixed;
